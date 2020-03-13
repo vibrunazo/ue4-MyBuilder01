df20d488cf9905a5f9c00225c511d3e41acebd59
Vandré Israel Brunazo <vibrunazo@gmail.com>
2020-03-13 17:56:38 -0300

    proj hits once

---
+++ Source/MyBuilder/HasHitbox.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HasHitbox.h"
+
+// Add default functionality here for any IHasHitbox functions that are not pure virtual.
---
+++ Source/MyBuilder/HasHitbox.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "HasHitbox.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UHasHitbox : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class MYBUILDER_API IHasHitbox
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Hitbox")
+	void OnHitSuccess();
+};
